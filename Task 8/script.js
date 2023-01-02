8ab5bcaf6adfcc16ae00a6d69c62058cca99dad3
ryckabass0827 <laiskasrichardui@gmail.com>
2023-01-02 11:42:42 +0200

    8 uzduotis padaryta

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,31 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    constructor() {
+        this.sum = this.sum.bind(this);
+        this.substraction = this.substraction.bind(this);
+        this.multiplication = this.multiplication.bind(this);
+        this.division = this.division.bind(this);
+    }
+
+    sum(a, b) {
+        return a + b;
+    }
+    substraction(a, b) {
+        return a - b;
+    }
+    multiplication(a, b) {
+        return a * b;
+    }
+    division(a, b) {
+        return a / b;
+    }
+}
+
+const calc = new Calculator();
+console.log('Sudetis', calc.sum(12, 13));
+console.log('Atimtis', calc.substraction(100, 25));
+console.log('Daugyba', calc.multiplication(7, 8));
+console.log('Dalyba', calc.division(1548, 138));
