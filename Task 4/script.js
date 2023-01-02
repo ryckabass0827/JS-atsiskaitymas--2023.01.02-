e57c54a57093d1e1055f3aa1d93ddfeec49730b9
ryckabass0827 <laiskasrichardui@gmail.com>
2023-01-02 10:42:41 +0200

    4 uzduotis padaryta

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,22 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+fetch('cars.json')
+    .then(response => response.json())
+    .then(data => {
+        data.sort((a, b) => a.brand.localeCompare(b.brand));
+        let output = document.getElementById("output");
+        let carCardsHTML = "";
+        data.forEach(car => {
+            carCardsHTML += `
+        <div class="carCard">
+          <h2 class="carBrand">${car.brand}</h2>
+          <ol class="carModels">
+            ${car.models.map(model => `<li class="car__model">${model}</li>`).join('')}
+          </ol>
+        </div>
+      `;
+        });
+
+        output.innerHTML = carCardsHTML;
+    });
